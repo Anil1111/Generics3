aaca2c85dd53761f1fa798a7638f5e59c599c809
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-06-10 08:32:49 +0530

    Generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Generics
-{
-    class Program
-    {
-        static void Main(string[] args)
+class GenericClass<T>
         {
+            public GenericClass(T msg)
+            {
+                Console.WriteLine(msg);
+            }
         }
-    }
-}
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                GenericClass<string> gen = new GenericClass<string>("This is My generic class");
+        GenericClass<double> getd = new GenericClass<double>(110.5);
+        GenericClass<bool> getB = new GenericClass<bool>(true);
+        GenericClass<int> getI = new GenericClass<int>(101);
+        GenericClass<char> getCh = new GenericClass<char>('I');
+        
+                
+            }
+        }
